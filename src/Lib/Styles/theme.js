edffd0ccc447d0582e72df580cd408eb2d9b0317
markomrkonjic479@gmail.com <markomrkonjic479@gmail.com>
2021-06-30 22:48:02 +0200

    Added some folders and lib folder

---
+++ src/Lib/Styles/theme.js
@@ -0,0 +1,12 @@
+export const breakpoints = {
+    mobileLarge: 'min-width: 576px',
+    tablet: 'min-width: 768px',
+    desktop: 'min-width: 1024px',
+    desktopLarge: 'min-width: 1300px',
+  };
+
+export const Colors = {
+    TransparentButton:` rgba(241,6,6,0)`,
+    TransparentInputBox: `rgba(0,0,0,0.45)`,
+    White:`#ffffff`
+};
